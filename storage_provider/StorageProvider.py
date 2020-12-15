504cac599b76c8f937ad9444d4099a7bb0b52ce9
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-12-15 13:20:52 +0100

    docs(storage-provider): add docstring to functions

--- storage_provider/StorageProvider.py
+++ storage_provider/StorageProvider.py
@@ -1,6 +1,12 @@
 class StorageProvider:
     def read(self, object_id):
+        """
+        Reads an object from the storage.
+        """
         pass
 
     def write(self, object_id, content):
+        """
+        Writes to an object in the storage.
+        """
         pass
